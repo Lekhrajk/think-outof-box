345bc4306025a0ff72d9982782e08bcec84097f3
LekhrajK <lrlucky6042@gmail.com>
2020-07-10 05:47:34 +0530

    Todo list added

--- Platonia assigmnent/todos/src/main.js
+++ Platonia assigmnent/todos/src/main.js
@@ -8,7 +8,6 @@ import jQuery from 'jquery';
 window.$ = window.jQuery = jQuery;
 import 'popper.js';
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
--- Platonia assigmnent/todos/src/views/Home.vue
+++ Platonia assigmnent/todos/src/views/Home.vue
@@ -55,6 +55,7 @@
     },
     mounted() {
       this.$store.dispatch('getTodos');
+      
     },
     components: {
       Card
